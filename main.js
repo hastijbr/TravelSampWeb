ee1838bc97f0911844a12662188fb191c0c1acf1
Hasti Jabbari <hastihj383@gmail.com>
2021-05-10 21:50:47 +0430

    First commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    
+    $('.menu').click(function(){
+        $('.navbar').toggle();
+        $('.menu .fa-bars').toggleClass('fa-times');
+        $('section').toggleClass('nav-toggle');
+    });
+});
